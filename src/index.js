168e4593da3a70718b1ce6687301fa5b35712651
Maxim <mihalenko.maxim21@gmail.com>
2023-03-16 15:08:24 +0300

    Add connection to database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 
+const db = require("./database");
+
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log("Server is running " + PORT);
-});
+const startApplication = async () => {
+  try {
+    await db.authenticate();
+    await db.sync();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port - ${PORT}`);
+    });
+  } catch (error) {}
+};
+
+startApplication();
